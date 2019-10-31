6e2ec5579d9730f424b5674c444c86b4e4850a39
O'Dwayne Irving <odwayne.irving@u.group>
2019-10-31 10:26:15 -0400

    Updated install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if sudo apt-get install unzip; then
   if wget https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip; then
-    unzip terraform_0.12.7_linux_amd64.zip
+    unzip terraform_0.12.12_linux_amd64.zip
     sudo mv terraform /usr/local/bin/
     terraform --version 
   fi
