3e24df81206ad4f8bf8d8ab3258f182d0e3a8b01
O'Dwayne Irving <odwayne.irving@u.group>
2019-10-31 10:22:23 -0400

    Updated hackerrank configs

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if sudo apt-get install unzip; then
+  if wget https://releases.hashicorp.com/terraform/0.12.12/terraform_0.12.12_linux_amd64.zip; then
+    unzip terraform_0.12.7_linux_amd64.zip
+    sudo mv terraform /usr/local/bin/
+    terraform --version 
+  fi
+fi
