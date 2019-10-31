9a96c8b1f7c22c3361d03d297867db57fa88a679
O'Dwayne Irving <odwayne.irving@u.group>
2019-10-31 10:06:04 -0400

    Add hello world index.html and correspondign Dockerfile. Updated score.sh

--- configs/score.sh
+++ configs/score.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+SCORE=0
+
+if terraform validate; then
+SCORE = SCORE + 45
+  if terraform plan -out plan; then
+  SCORE = SCORE + 15
+    if terraform apply plan; then
+      SCORE = SCORE + 15
+    fi
+  fi
+fi
+
